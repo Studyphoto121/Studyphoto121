98551a813648ce8434eb4a91a7195cddd441af17
Studyphoto121 <93310696+Studyphoto121@users.noreply.github.com>
2023-04-21 19:32:27 +0700

    Create yoyomoi.lua

---
+++ yoyomoi.lua
@@ -0,0 +1,15 @@
+local LocalPlayer = game:GetService("Players").LocalPlayer
+local Account = loadstring(game:HttpGet"https://raw.githubusercontent.com/ic3w0lf22/Roblox-Account-Manager/master/RAMAccount.lua")()
+local Acc = Account.new(LocalPlayer.Name)
+local Library = require(game.ReplicatedStorage.Library)
+Library.Load()
+
+local saveData = Library.Save.Get()
+local currencyLeft = saveData[CURRENCY_NAME]
+
+local currencyString = Library.Functions.NumberShorten(currencyLeft) .. " "  .. G --load diamond
+
+local __aliasText = ""
+
+__aliasText = currencyString
+Acc:SetAlias(__aliasText)
