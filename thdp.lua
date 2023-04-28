c69627b9724f823906f3f38ede228bcb2f1d0c04
Studyphoto121 <93310696+Studyphoto121@users.noreply.github.com>
2023-04-28 19:35:46 +0700

    Update thdp.lua
    fweq

--- thdp.lua
+++ thdp.lua
@@ -5,7 +5,7 @@ local CurrentCamera = workspace.CurrentCamera
 local newindex; newindex = hookmetamethod(game, '__newindex', function(obj, idx, val)
 
    if obj == CurrentCamera and idx == 'CFrame' then
-       val = val + (val.LookVector * -7)
+       val = val + (val.LookVector * _G.thrdrange)
    end
 
    return newindex(obj, idx, val)
