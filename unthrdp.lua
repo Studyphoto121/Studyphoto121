b28bd105b1c2d852f80b04fde478b2c3003311de
Studyphoto121 <93310696+Studyphoto121@users.noreply.github.com>
2023-04-28 13:49:58 +0700

    egg

---
+++ unthrdp.lua
@@ -0,0 +1,12 @@
+local LocalPlayer = game.Players.LocalPlayer
+local CurrentCamera = workspace.CurrentCamera
+
+--// Script
+local newindex; newindex = hookmetamethod(game, '__newindex', function(obj, idx, val)
+
+   if obj == CurrentCamera and idx == 'CFrame' then
+       val = val + (val.LookVector * 7)
+   end
+
+   return newindex(obj, idx, val)
+end)
