5ba4467800d3eeb17875560ad9d0d929802a1525
Studyphoto121 <93310696+Studyphoto121@users.noreply.github.com>
2023-09-09 15:03:30 +0700

    Update thdp.lua

--- thdp.lua
+++ thdp.lua
@@ -1,18 +1,9 @@
-_G.runny = _G.thrdrange * 2
+local distance = 10
 
-_G.gunny = _G.thrdrange - _G.runny
-
-ez = _G.gunny
-
-local LocalPlayer = game.Players.LocalPlayer
-local CurrentCamera = workspace.CurrentCamera
-
---// Script
-local newindex; newindex = hookmetamethod(game, '__newindex', function(obj, idx, val)
-
-   if obj == CurrentCamera and idx == 'CFrame' then
-       val = val + (val.LookVector * -7)
-   end
-
-   return newindex(obj, idx, val)
+local newindex; newindex = hookmetamethod(game, "__newindex", function(self, key, value)
+    if tostring(self) == "Camera" and key == "CFrame" then
+        value = value * CFrame.new(0, 0, distance)
+    end
+   
+    return newindex(self, key, value)
 end)
