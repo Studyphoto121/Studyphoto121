aba2dc999451be1013fb0adbacfdc2ede18dfebd
Studyphoto121 <93310696+Studyphoto121@users.noreply.github.com>
2024-05-12 12:35:17 -0700

    Update kcheck.lua

--- kcheck.lua
+++ kcheck.lua
@@ -1,7 +1,17 @@
 local keys = 'CKFK9-QNGF2-D34FM-99QX3-8XC4K'
 
 if _G.Key == keys then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Studyphoto121/Studyphoto121/main/Script.lua"))()
+    local versionURL
+    if _G.Version == "private" then
+        versionURL = "https://raw.githubusercontent.com/Studyphoto121/Studyphoto121/main/Script.lua"
+    elseif _G.Version == "cupcake" then
+        versionURL = "https://raw.githubusercontent.com/Studyphoto121/Studyphoto121/main/thdp.lua"
+    else
+        print("Invalid version")
+        return
+    end
+    
+    loadstring(game:HttpGet(versionURL))()
 else
-    print("nawh")
+    print("Invalid key")
 end
