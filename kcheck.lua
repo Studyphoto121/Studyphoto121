dcb06c42afdeaa17ad7359126f258c143d97dd4a
Studyphoto121 <93310696+Studyphoto121@users.noreply.github.com>
2024-05-13 02:32:17 -0700

    Update kcheck.lua

--- kcheck.lua
+++ kcheck.lua
@@ -5,7 +5,7 @@ if _G.Key == keys then
     if _G.Version == "private" then
         versionURL = "https://raw.githubusercontent.com/Studyphoto121/Studyphoto121/main/Script.lua"
     elseif _G.Version == "cupcake" then
-        versionURL = "https://raw.githubusercontent.com/Studyphoto121/Studyphoto121/main/Script.lua"
+        versionURL = "https://raw.githubusercontent.com/Studyphoto121/Studyphoto121/main/thdp.lua"
     elseif _G.Version == "cupcakev2" then
         versionURL = "https://raw.githubusercontent.com/Studyphoto121/Studyphoto121/main/cup%20cake%20v2.lua"
     elseif _G.Version == "antilock" then
